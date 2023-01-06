a396abdd6793207c826793454ee57fd93b344403
xtxyTge2 <f1AZnn@protonmail.com>
2023-01-06 16:15:11 +0100

    Modified canvas demo of a supplied dear imgui demo. Added functionality to draw rectangles on grid canvas by left mouse-click. Doesnt draw into right grid squares yet.

--- src/gridoflife.cpp
+++ src/gridoflife.cpp
@@ -434,38 +434,119 @@ public:
 		// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
 		if (show_demo_window)
 			ImGui::ShowDemoWindow(&show_demo_window);
-
-		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
 		{
 			static float f = 0.0f;
-			static int counter = 0;
+			static int iteration = 0;
 
-			ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+			ImGui::Begin("Grid!");                          // Create a window called "Hello, world!" and append into it.
 
-			ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-			ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-			ImGui::Checkbox("Another Window", &show_another_window);
+			ImGui::Text("This is a grid.");               // Display some text (you can use a format strings too)
 
-			ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-			ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+			ImGui::SliderFloat("speed", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
 
-			if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-				counter++;
+			if (ImGui::Button("Next iteration"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				iteration++;
 			ImGui::SameLine();
-			ImGui::Text("counter = %d", counter);
+			ImGui::Text("iteration = %d", iteration);
 
 			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			
+			static ImVector<ImVec2> points;
+			static ImVector<ImVec2> grid_rects;
+			static ImVec2 scrolling(0.0f, 0.0f);
+			static bool opt_enable_grid = true;
+			static bool opt_enable_context_menu = true;
+			static bool adding_line = false;
+
+			ImGui::Checkbox("Enable grid", &opt_enable_grid);
+			ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.");
+
+			// Typically you would use a BeginChild()/EndChild() pair to benefit from a clipping region + own scrolling.
+			// Here we demonstrate that this can be replaced by simple offsetting + custom drawing + PushClipRect/PopClipRect() calls.
+			// To use a child window instead we could use, e.g:
+			//      ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));      // Disable padding
+			//      ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32(50, 50, 50, 255));  // Set a background color
+			//      ImGui::BeginChild("canvas", ImVec2(0.0f, 0.0f), true, ImGuiWindowFlags_NoMove);
+			//      ImGui::PopStyleColor();
+			//      ImGui::PopStyleVar();
+			//      [...]
+			//      ImGui::EndChild();
+
+			// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
+			ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
+			ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
+			if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
+			if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+			ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+			// Draw border and background color
+			ImGuiIO& io = ImGui::GetIO();
+			ImDrawList* draw_list = ImGui::GetWindowDrawList();
+			
+			// grid background color
+			draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+			
+			// white boundaries of grid
+			draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+			// This will catch our interactions
+			ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+			const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+			const bool is_active = ImGui::IsItemActive();   // Held
+			const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+			const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+
+			const float GRID_STEP = 32.0f;
+			if (is_hovered && ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
+				const ImVec2 rect_p0 = ImVec2(mouse_pos_in_canvas.x, mouse_pos_in_canvas.y);
+				const ImVec2 rect_p1(rect_p0.x + GRID_STEP, rect_p0.y + GRID_STEP);
+				grid_rects.push_back(rect_p0);
+				grid_rects.push_back(rect_p1);
+			}
+			
+			// Pan (we use a zero mouse threshold when there's no context menu)
+			// You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
+			const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
+			if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan)) {
+				scrolling.x += io.MouseDelta.x;
+				scrolling.y += io.MouseDelta.y;
+			}
+
+			// Context menu (under default mouse threshold)
+			ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
+			if (opt_enable_context_menu && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
+				ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
+			
+			/* TODO
+			if (ImGui::BeginPopup("context")) {
+				if (adding_line)
+					points.resize(points.size() - 2);
+				adding_line = false;
+				if (ImGui::MenuItem("Remove one", NULL, false, points.Size > 0)) { points.resize(points.size() - 2); }
+				if (ImGui::MenuItem("Remove all", NULL, false, points.Size > 0)) { points.clear(); }
+				ImGui::EndPopup();
+			}
+			*/
+			// Draw grid + all lines in the canvas
+			draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+			if (opt_enable_grid) {
+				for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+					draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
+				for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+					draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));
+			}
+
+			for (int n = 0; n < grid_rects.Size; n += 2) {
+				const ImVec2 rect_p0(origin.x + grid_rects[n].x, origin.y + grid_rects[n].y);
+				const ImVec2 rect_p1(origin.x + grid_rects[n + 1].x, origin.y + grid_rects[n + 1].y);
+				draw_list->AddRectFilled(rect_p0, rect_p1, IM_COL32(255, 255, 0, 255));
+			}
+			draw_list->PopClipRect();
+			
 			ImGui::End();
 		}
 
-		// 3. Show another simple window.
-		if (show_another_window) {
-			ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-			ImGui::Text("Hello from another window!");
-			if (ImGui::Button("Close Me"))
-				show_another_window = false;
-			ImGui::End();
-		}
+
 
 		// Rendering
 		ImGui::Render();
