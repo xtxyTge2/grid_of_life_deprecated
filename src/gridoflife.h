e77858f5889f0eec908d6473f0d737637ed177d7
xtxyTge2 <f1AZnn@protonmail.com>
2023-01-06 12:46:21 +0100

    Added 3rd-party dependencies, namely sdl2 and dear imgui. Also added cmake files to build the project.

---
+++ src/gridoflife.h
@@ -0,0 +1,8 @@
+﻿// GridOfLife.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+#include <iostream>
+#include <vector>
+#include "imgui.h"
+// TODO: Reference additional headers your program requires here.
